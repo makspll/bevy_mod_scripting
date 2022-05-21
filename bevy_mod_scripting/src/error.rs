07c8daf8ff16aa6f19d862a80a6aa6e64c0f107b
makspll <makspl17@gmail.com>
2022-05-21 15:09:39 +0100

    improved error messages

--- bevy_mod_scripting/src/error.rs
+++ bevy_mod_scripting/src/error.rs
@@ -4,27 +4,27 @@ use thiserror::Error;
 
 #[derive(Error,Debug)]
 pub enum ScriptError {
-    #[error("Runtime error in script `{script:?}` {msg:?}")]
+    #[error("Runtime error in script `{script}` {msg}")]
     RuntimeError{
         script: String,
         msg: String,
     },
-    #[error("Failed to load script asset for `{script:?}`")]
+    #[error("Failed to load script asset for `{script}`")]
     FailedToLoad{
         script: String
     },
-    #[error("Syntax error for script `{script:?}` {msg:?}")]
+    #[error("Syntax error for script `{script}` {msg}")]
     SyntaxError{
         script: String,
         msg : String
     },
-    #[error("Callback method `{callback:?}` invalid for script `{script:?}` {msg:?}")]
+    #[error("Callback method `{callback}` invalid for script `{script}` {msg}")]
     InvalidCallback{
         script: String,
         callback: String,
         msg: String,
     },
-    #[error("{0:?}")]
+    #[error("{0}")]
     Other(String),
 }
 
