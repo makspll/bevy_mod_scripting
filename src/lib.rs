a4404875d7d95a7706b96b373122b21e857d387c
makspll <makspl17@gmail.com>
2022-08-10 12:16:45 +0100

    rename feature

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub mod core {
 pub mod lua {
     pub use bevy_mod_scripting_lua::*;
 
-    #[cfg(feature="lua_bevy_api")]
+    #[cfg(feature="lua_script_api")]
     pub mod api {
         pub use bevy_script_api::lua::*;
     }
@@ -18,14 +18,14 @@ pub mod lua {
 pub mod rhai {
     pub use bevy_mod_scripting_rhai::*;
 
-    #[cfg(feature="rhai_bevy_api")]
+    #[cfg(feature="rhai_script_api")]
     pub mod api {
     //     pub use bevy_script_api::rhai::*;
     }
 }
 
 
-#[cfg(any(feature="lua_bevy_api",feature="rhai_bevy_api"))]
+#[cfg(any(feature="lua_script_api",feature="rhai_script_api"))]
 pub mod api {
     pub use bevy_script_api::*;
 }
