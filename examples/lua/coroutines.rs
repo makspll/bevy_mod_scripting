ee30cf12f87151a56a226cec086c05b0391c40cf
Maksymilian Mozolewski <makspl17@gmail.com>
2022-09-22 12:15:42 +0100

    add coroutine example (#33)

---
+++ examples/lua/coroutines.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+use bevy_mod_scripting::prelude::*;
+
+/// fire on_update
+fn do_update(mut w: PriorityEventWriter<LuaEvent<()>>, time: Res<Time>) {
+    let event = LuaEvent {
+        hook_name: "on_update".to_owned(),
+        args: (),
+        recipients: Recipients::All,
+    };
+    info!(
+        "\t - event: {}, time: {:?}",
+        event.hook_name,
+        (time.startup() + time.time_since_startup())
+    );
+    w.send(event, 0);
+}
+
+fn load_our_scripts(server: Res<AssetServer>, mut commands: Commands) {
+    let path = "scripts/coroutines.lua";
+    let handle = server.load::<LuaFile, &str>(path);
+    let script = Script::<LuaFile>::new(path.to_string(), handle);
+
+    commands.spawn().insert(ScriptCollection::<LuaFile> {
+        scripts: vec![script],
+    });
+}
+
+fn main() -> std::io::Result<()> {
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins)
+        .add_plugin(ScriptingPlugin)
+        .add_startup_system(load_our_scripts)
+        // randomly fire events for either all scripts,
+        // the script with id 0
+        // or the script with id 1
+        .add_system(do_update)
+        .add_script_handler_stage::<LuaScriptHost<()>, _, 0, 0>(CoreStage::PostUpdate)
+        .add_script_host::<LuaScriptHost<()>, _>(CoreStage::PostUpdate);
+    app.run();
+
+    Ok(())
+}
