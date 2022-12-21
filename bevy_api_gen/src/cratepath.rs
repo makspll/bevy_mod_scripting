4795c74c5aa9fe592256db43885407d372b80d7a
makspll <makspl17@gmail.com>
2022-12-21 02:54:34 +0000

    fix certain paths not being found + minor improvements

--- bevy_api_gen/src/cratepath.rs
+++ bevy_api_gen/src/cratepath.rs
@@ -1,58 +1,42 @@
 use rustdoc_types::{Crate, Id, ItemEnum, Visibility};
 
-pub(crate) fn get_path(id: &Id, source: &Crate, verbose: bool) -> Option<Vec<Id>> {
-    let verbose = verbose;// || (id == &Id(String::from("0:6568:5720")));
-    if verbose {
-        eprintln!("Search for {:?}", id);
-    }
+pub(crate) fn get_path(id: &Id, source: &Crate) -> Option<Vec<Id>> {
     match source.paths.get(id) {
         Some(_) => return Some(vec![id.to_owned()]),
         None => {
             let ind = source.index.get(id)?;
-            if let Visibility::Restricted { parent, path: _ } = &ind.visibility {
-                if let Some(p_path) = get_path(parent, source, verbose) {
-                    
-                    if verbose {
-                        eprintln!("restrict: {:?}", p_path);
-                    }
+            if let Visibility::Restricted { parent, .. } = &ind.visibility {
+                if let Some(p_path) = get_path(parent, source) {
                     return Some(p_path);
                 }
             }
-            let parents = (&source.index)
-                .into_iter()
-                .filter(|(_, p_item)| {
-                    if let Some(name) = &ind.name {
-                        if p_item.links.contains_key(name) {
-                            return true;
-                        }
+            let parents = (&source.index).into_iter().filter(|(_, p_item)| {
+                if let Some(name) = &ind.name {
+                    if p_item.links.contains_key(name) {
+                        return true;
                     }
-                    if let ItemEnum::Impl(p_impl) = &p_item.inner {
-                        return p_impl.items.contains(id);
-                    }
-                    if let ItemEnum::Import(p_import) = &p_item.inner {
-                        if let Some(p_inner) = &p_import.id {
-                            return p_inner == id;
-                        }
-                        return false;
-                    }
-                    if let ItemEnum::Module(p_mod) = &p_item.inner {
-                        return p_mod.items.contains(id);
+                }
+                if let ItemEnum::Impl(p_impl) = &p_item.inner {
+                    return p_impl.items.contains(id);
+                }
+                if let ItemEnum::Import(p_import) = &p_item.inner {
+                    if let Some(p_inner) = &p_import.id {
+                        return p_inner == id;
                     }
                     return false;
-                });
+                }
+                if let ItemEnum::Module(p_mod) = &p_item.inner {
+                    return p_mod.items.contains(id);
+                }
+                false
+            });
 
             for (parent, _) in parents {
-                if verbose {}
-                let path_o = get_path(parent, source, verbose);
+                let path_o = get_path(parent, source);
                 if let Some(mut path) = path_o {
                     path.push(id.to_owned());
                     return Some(path);
-                } else if verbose {
-                    eprintln!("{:?}", parent);
                 }
-            }
-            if verbose {
-                eprintln!("no match");
             }
         }
     };
@@ -60,35 +44,33 @@ pub(crate) fn get_path(id: &Id, source: &Crate, verbose: bool) -> Option<Vec<Id>
 }
 
 pub(crate) fn path_to_import(path: Vec<Id>, source: &Crate) -> Vec<String> {
-    /* 
-    for i in path {
-        eprintln!("{:?}", source.index.get(&i).unwrap());
-    }
-    eprintln!();
-    
-    if path.last().unwrap().0 == String::from("0:3728:1594") {
-        eprintln!("{:?}",path);
-        eprintln!("{:?}", source.index.get(path.last().unwrap()).unwrap());
-    }
-    */
-    path.iter().rev().enumerate().rev().enumerate().map(|(starti, (endi, id))| {
-        let ind = source.index.get(id).unwrap();
-        if starti == 0 {
-            return source.paths.get(id).unwrap().path.clone();
-        }
-        else if endi==0 {
-            if let Some(name) = &ind.name {
-                return vec![name.to_owned()];
+    path.iter()
+        .rev()
+        .enumerate()
+        .rev()
+        .enumerate()
+        .map(|(starti, (endi, id))| {
+            let ind = source.index.get(id).unwrap();
+            if starti == 0 {
+                return source.paths.get(id).unwrap().path.clone();
+            } else if endi == 0 {
+                if let Some(name) = &ind.name {
+                    return vec![name.to_owned()];
+                }
+            } else if let Visibility::Restricted { parent: _, path } = &ind.visibility {
+                return path[2..].split("::").map(|x| x.to_string()).collect();
+            } else if let ItemEnum::Module(module) = &ind.inner {
+                if !module.is_stripped {
+                    return vec![source.index.get(id).unwrap().name.clone().unwrap()];
+                } else {
+                    return vec![];
+                }
             }
-        }
-        else if let Visibility::Restricted { parent: _, path } = &ind.visibility {
-            return path[2..].split("::").map(|x| x.to_string()).collect();
-        }
-        else if let ItemEnum::Module(_) = &ind.inner {
-            //eprintln!("{:?}\n", ind);
-            //return vec![ind.name.to_owned().unwrap()];
-            return vec![];
-        }
-        vec![]
-    }).reduce(|mut x,y| {x.extend(y); x}).unwrap()
+            vec![]
+        })
+        .reduce(|mut x, y| {
+            x.extend(y);
+            x
+        })
+        .unwrap()
 }
