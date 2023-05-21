786b552deaf25bd958668c66dfc0bb99a82e961b
Dan Vail <danielhvail@live.com>
2023-05-21 18:11:53 -0500

    First check in for adding Rhai support

--- languages/bevy_mod_scripting_rhai_derive/src/lib.rs
+++ languages/bevy_mod_scripting_rhai_derive/src/lib.rs
@@ -1,15 +1,21 @@
+use bevy_mod_scripting_common::{implementor::WrapperImplementor, newtype::Newtype};
+use implementor::RhaiImplementor;
 use proc_macro::TokenStream;
+use syn::parse_macro_input;
 
 pub(crate) mod rhai_method;
+pub(crate) mod derive_flags;
+pub(crate) mod implementor;
+pub(crate) mod impls;
 
 #[proc_macro]
-pub fn impl_lua_newtype(tokens: TokenStream) -> TokenStream {
-    // let newtype = parse_macro_input!(tokens as Newtype);
+pub fn impl_rhai_newtype(tokens: TokenStream) -> TokenStream {
+    let newtype = parse_macro_input!(tokens as Newtype);
+    let mut implementor = LuaImplementor::default();
 
-    // implementor
-    //     .generate(newtype)
-    //     .map_err(|e| e.to_compile_error())
-    //     .unwrap_or_else(core::convert::identity)
-    //     .into()
-    tokens
+    implementor
+        .generate(newtype)
+        .map_err(|e| e.to_compile_error())
+        .unwrap_or_else(core::convert::identity)
+        .into()
 }
--- languages/bevy_mod_scripting_rhai_derive/src/rhai_method.rs
+++ languages/bevy_mod_scripting_rhai_derive/src/rhai_method.rs
@@ -1,29 +1,113 @@
-use bevy_mod_scripting_common::implementor::WrapperFunction;
+use bevy_mod_scripting_common::utils::{attribute_to_string_lit, EmptyToken};
 use proc_macro2::TokenStream;
-use quote::ToTokens;
+use quote::{ToTokens, quote_spanned};
 use syn::{
     parse::{Parse, ParseStream},
+    punctuated::Punctuated,
+    spanned::Spanned,
+    token::Paren,
     *,
 };
-impl WrapperFunction for RhaiMethod {}
 
-pub(crate) struct RhaiMethod {
+// Because of the possibility wherein we define the meta method 
+// type as both mutuable and static, this enum was created
+// to ensure that an invalid state is not representable.
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+pub(crate) enum RhaiMethodMetaType {
+    Static,
+    MutableSelf,
+    ImmutableSelf,
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+pub(crate) enum RhaiFieldFunction {
+    Setter,
+    Getter,
+    Neither,
+}
+
+#[derive(PartialEq, Eq, Clone, Hash, Debug)]
+pub(crate) struct RhaiMethodType {
+    pub is_function: bool,
+    pub field_function: RhaiFieldFunction,
+    pub method_type: RhaiMethodMetaType,
+    pub method_name: String,
     pub method: ItemFn,
 }
 
-impl Parse for RhaiMethod {
+impl RhaiMethodType {
+    pub fn get_inner_tokens(&self) -> TokenStream {
+        self.method_name.into_token_stream()
+    }
+
+    pub fn is_field(&self) -> bool {
+        self.field_function != RhaiFieldFunction::Neither 
+    }
+
+    pub fn is_static(&self) -> bool {
+        self.method_type == RhaiMethodMetaType::Static
+    }
+    
+    pub fn is_mut(&self) -> bool {
+        self.method_type == RhaiMethodMetaType::MutableSelf
+    }
+}
+
+impl ToTokens for RhaiMethodType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let meta_type = self.method_type;
+    }
+}
+
+impl Parse for RhaiMethodType {
     fn parse(input: ParseStream) -> Result<Self> {
         Ok(Self {
+            is_function: false,
+            field_function: RhaiFieldFunction::Setter,
+            method_type: RhaiMethodMetaType::Static,
+            method_name: String::from("yeet"),
             method: input.parse()?,
         })
     }
 }
 
+pub(crate) struct RhaiClosure {
+    arrow: Token![=>],
+    expr: ExprClosure,
+}
+
+impl RhaiClosure {
+    pub fn to_applied_closure(&self) -> TokenStream {
+        let expr = &self.expr;
+        quote_spanned! {self.span()=>
+            #expr
+        }
+    }
+}
+
+impl ToTokens for RhaiClosure {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let arrow = &self.arrow;
+        let expr = &self.expr;
+        tokens.extend(quote::quote! {
+            #arrow #expr
+        });
+    }
+}
+
+pub(crate) struct RhaiMethod {
+    pub docstring: Vec<Attribute>,
+    pub method_type: RhaiMethodType,
+    pub closure: RhaiClosure,
+}
+
 impl ToTokens for RhaiMethod {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let method = &self.method;
+        let ds: Punctuated<Attribute, EmptyToken> = self.docstring.iter().cloned().collect();
+        let mt = self.method_type;
+        let method = &self.closure;
         tokens.extend(quote::quote! {
-            #method
+            #ds #mt #method
         })
     }
 }
