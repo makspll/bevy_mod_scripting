0ad1a94d4626c4d70e3885e0c0de2a2f9edea34a
Tim Irmler <zwazel@hotmail.com>
2023-10-12 18:58:07 +0200

    using the ScriptLoaded event

--- examples/rhai/multiple_events_rhai.rs
+++ examples/rhai/multiple_events_rhai.rs
@@ -1,5 +1,6 @@
 use bevy::{prelude::*, reflect::Reflect};
 use bevy_mod_scripting_core::{
+    event::ScriptLoaded,
     prelude::{APIProvider, PriorityEventWriter, Recipients, Script, ScriptCollection},
     AddScriptApiProvider, AddScriptHost, AddScriptHostHandler, ScriptingPlugin,
 };
@@ -106,28 +107,32 @@ fn call_update(
 
 fn call_init(
     mut events: PriorityEventWriter<RhaiEvent<ScriptArgs>>,
+    mut loaded_scripts: EventReader<ScriptLoaded>,
+    entity_query: Query<(Entity, Option<&Name>, Option<&ScriptCollection<RhaiFile>>)>,
     mut commands: Commands,
-    entity_query: Query<
-        (Entity, Option<&Name>, Option<&ScriptCollection<RhaiFile>>),
-        Added<NewlyAddedEntityCallInit>,
-    >,
 ) {
-    entity_query.for_each(|(entity, name, scripts)| {
-        if scripts.is_some() {
-            events.send(
-                RhaiEvent {
-                    hook_name: "on_init".to_owned(),
-                    args: ScriptArgs {
-                        delta_time: None,
-                        entity_name: name.map(|n| n.to_string()),
-                    },
-                    recipients: Recipients::Entity(entity),
-                },
-                0,
-            );
-            commands.entity(entity).remove::<NewlyAddedEntityCallInit>();
-        } else {
+    // Call init on all entities that have a script that was just loaded, and remove the Marker component, so that Update can be called
+    'outer: for loaded_script in loaded_scripts.iter() {
+        for (entity, name, scripts) in entity_query.iter() {
+            if let Some(scripts) = scripts {
+                if scripts.scripts.iter().any(|s| s.id() == loaded_script.sid) {
+                    events.send(
+                        RhaiEvent {
+                            hook_name: "on_init".to_owned(),
+                            args: ScriptArgs {
+                                entity_name: name.map(|n| n.to_string()),
+                                ..Default::default()
+                            },
+                            recipients: Recipients::ScriptID(loaded_script.sid),
+                        },
+                        0,
+                    );
+
+                    continue 'outer;
+                }
+            }
+
             commands.entity(entity).remove::<NewlyAddedEntityCallInit>();
         }
-    });
+    }
 }
