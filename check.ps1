1d578d43bc1c34efb77d47d60cccac44872b5be8
makspll <makspl17@gmail.com>
2025-01-02 17:03:22 +0000

    update check.ps1 and generate exe

--- check.ps1
+++ check.ps1
@@ -5,5 +5,5 @@ $CURRENT_DIR = Split-Path -Leaf -Path (Get-Location)
 if ($CURRENT_DIR -eq "bevy_api_gen") {
     cargo +nightly-2024-12-15 clippy --all-targets --message-format=json
 } else {
-    cargo clippy --workspace --all-targets --message-format=json --features="lua54 rhai teal rune bevy/file_watcher bevy/multi_threaded"
+    cargo clippy --workspace --all-targets --message-format=json --features="lua54 rhai rune bevy/file_watcher bevy/multi_threaded"
 }
