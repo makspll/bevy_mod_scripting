eb9b36c7b650f50083fda6576f0e52db014c0cfb
Joaquín León <51842726+Joakker@users.noreply.github.com>
2025-02-25 16:23:04 -0300

    fix: Generate IntoScript implementation with the correct path (#326)

--- crates/bevy_mod_scripting_derive/src/derive/into_script.rs
+++ crates/bevy_mod_scripting_derive/src/derive/into_script.rs
@@ -13,7 +13,7 @@ pub fn into_script(input: TokenStream) -> TokenStream {
     let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
 
     quote! {
-        impl #impl_generics ::bevy_mod_scripting::bindings::function::into::IntoScript for #ident #type_generics #where_clause {
+        impl #impl_generics ::bevy_mod_scripting::core::bindings::function::into::IntoScript for #ident #type_generics #where_clause {
             fn into_script(self, world: ::bevy_mod_scripting::core::bindings::WorldGuard) -> Result<::bevy_mod_scripting::core::bindings::script_value::ScriptValue, ::bevy_mod_scripting::core::error::InteropError> {
                 ::bevy_mod_scripting::core::bindings::function::into::IntoScript::into_script(
                     ::bevy_mod_scripting::core::bindings::function::from::Val(self),
