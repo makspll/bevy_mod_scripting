1b540acc19d3305c9140f4ee81cab13b85045e40
makspll <makspl17@gmail.com>
2025-02-26 08:44:42 +0000

    feat: support setting hashmaps via reflection

--- crates/languages/bevy_mod_scripting_lua/tests/data/set/set_primitives_works.lua
+++ crates/languages/bevy_mod_scripting_lua/tests/data/set/set_primitives_works.lua
@@ -6,23 +6,29 @@ resource.bool = true
 resource.int = 42
 resource.float = 3.0
 resource.vec_usize = { 1, 2 }
+resource.string_map = { foo = "hello", zoo = "world" }
 
 assert(resource.string == "Hello, World!", "Expected 'Hello, World!', got " .. resource.string)
 assert(resource.bool == true, "Expected true, got " .. tostring(resource.bool))
 assert(resource.int == 42, "Expected 42, got " .. resource.int)
 assert(resource.float == 3.0, "Expected 3.14, got " .. resource.float)
 assert(resource.vec_usize[1] == 1, "Expected 1, got " .. resource.vec_usize[1])
+assert(resource.string_map:len() == 2, "Expected 2, got " .. resource.string_map:len())
+-- assert(resource.string_map["foo"] == "hello", "Expected 'hello', got " .. resource.string_map["foo"])
+-- assert(resource.string_map["zoo"] == "world", "Expected 'world', got " .. resource.string_map["zoo"])
 
 resource.string = "Goodbye, World!"
 resource.bool = false
 resource.int = 24
 resource.float = 1.0
 resource.vec_usize = { 3, 4 }
+resource.string_map = { foo = "goodbye", zoo = "world" }
 
 assert(resource.string == "Goodbye, World!", "Expected 'Goodbye, World!', got " .. resource.string)
 assert(resource.bool == false, "Expected false, got " .. tostring(resource.bool))
 assert(resource.int == 24, "Expected 24, got " .. resource.int)
 assert(resource.float == 1.0, "Expected 1.41, got " .. resource.float)
-assert(resource.vec_usize[1] == 3, "Expected 3, got " .. resource.vec_usize[1])
-
+assert(resource.string_map:len() == 2, "Expected 2, got " .. resource.string_map:len())
+-- assert(resource.string_map["foo"] == "goodbye", "Expected 'goodbye', got " .. resource.string_map["foo"])
+-- assert(resource.string_map["zoo"] == "world", "Expected 'world', got " .. resource.string_map["zoo"])
 
