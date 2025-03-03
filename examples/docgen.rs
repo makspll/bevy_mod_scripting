63b19142723f008760029aa88d2f70a2ead15233
makspll <makspl17@gmail.com>
2025-03-03 17:18:35 +0000

    dont wait for finalize just do it

--- examples/docgen.rs
+++ examples/docgen.rs
@@ -1,7 +1,10 @@
+use bevy::ecs::reflect::AppTypeRegistry;
 use bevy::{app::App, DefaultPlugins};
 use bevy_mod_scripting::ScriptFunctionsPlugin;
+use bevy_mod_scripting_core::bindings::function::script_function::AppScriptFunctionRegistry;
 use bevy_mod_scripting_core::bindings::globals::core::CoreScriptGlobalsPlugin;
-use ladfile_builder::plugin::ScriptingDocgenPlugin;
+use bevy_mod_scripting_core::bindings::globals::AppScriptGlobalsRegistry;
+use ladfile_builder::plugin::{generate_lad_file, LadFileSettings, ScriptingDocgenPlugin};
 
 fn main() -> std::io::Result<()> {
     let mut app = App::new();
@@ -17,10 +20,45 @@ fn main() -> std::io::Result<()> {
         ScriptFunctionsPlugin,
     ));
 
+    // there are two ways to generate the ladfile
+
+    // 1. add the docgen plugin and run your app as normal
     app.add_plugins(ScriptingDocgenPlugin::default());
-    app.cleanup();
     app.finish();
-    app.update();
+    app.cleanup();
+    // running update once will do the trick
+    // app.update();
+
+    // or 2. manually trigger the system
+    // this is what we do here as we're running this example in GHA
+
+    let type_registry = app
+        .world()
+        .get_resource::<AppTypeRegistry>()
+        .unwrap()
+        .clone();
+    let function_registry = app
+        .world()
+        .get_resource::<AppScriptFunctionRegistry>()
+        .unwrap()
+        .clone();
+    let global_registry = app
+        .world()
+        .get_resource::<AppScriptGlobalsRegistry>()
+        .unwrap()
+        .clone();
+
+    let settings = LadFileSettings {
+        description: "Core BMS framework bindings",
+        ..Default::default()
+    };
+
+    generate_lad_file(
+        &type_registry,
+        &function_registry,
+        &global_registry,
+        &settings,
+    );
 
     // bah bye, the generated file will be found in assets/
     // this can then be passed to various backends to generate docs, and other declaration files
