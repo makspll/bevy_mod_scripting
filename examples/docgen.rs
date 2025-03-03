290377d77bafdfafd3a3678e0d619e1f9781fc12
makspll <makspl17@gmail.com>
2025-03-03 17:05:17 +0000

    chore: fix docgen example

--- examples/docgen.rs
+++ examples/docgen.rs
@@ -1,10 +1,7 @@
-use bevy::ecs::reflect::AppTypeRegistry;
 use bevy::{app::App, DefaultPlugins};
 use bevy_mod_scripting::ScriptFunctionsPlugin;
-use bevy_mod_scripting_core::bindings::function::script_function::AppScriptFunctionRegistry;
 use bevy_mod_scripting_core::bindings::globals::core::CoreScriptGlobalsPlugin;
-use bevy_mod_scripting_core::bindings::globals::AppScriptGlobalsRegistry;
-use ladfile_builder::plugin::{generate_lad_file, LadFileSettings, ScriptingDocgenPlugin};
+use ladfile_builder::plugin::ScriptingDocgenPlugin;
 
 fn main() -> std::io::Result<()> {
     let mut app = App::new();
@@ -20,45 +17,10 @@ fn main() -> std::io::Result<()> {
         ScriptFunctionsPlugin,
     ));
 
-    // there are two ways to generate the ladfile
-
-    // 1. add the docgen plugin and run your app as normal
     app.add_plugins(ScriptingDocgenPlugin::default());
-    // running the app once like below would do the trick
-    // app.cleanup();
-    // app.finish();
-    // app.update();
-
-    // or 2. manually trigger the system
-    // this is what we do here as we're running this example in GHA
-
-    let type_registry = app
-        .world()
-        .get_resource::<AppTypeRegistry>()
-        .unwrap()
-        .clone();
-    let function_registry = app
-        .world()
-        .get_resource::<AppScriptFunctionRegistry>()
-        .unwrap()
-        .clone();
-    let global_registry = app
-        .world()
-        .get_resource::<AppScriptGlobalsRegistry>()
-        .unwrap()
-        .clone();
-
-    let settings = LadFileSettings {
-        description: "Core BMS framework bindings",
-        ..Default::default()
-    };
-
-    generate_lad_file(
-        &type_registry,
-        &function_registry,
-        &global_registry,
-        &settings,
-    );
+    app.cleanup();
+    app.finish();
+    app.update();
 
     // bah bye, the generated file will be found in assets/
     // this can then be passed to various backends to generate docs, and other declaration files
