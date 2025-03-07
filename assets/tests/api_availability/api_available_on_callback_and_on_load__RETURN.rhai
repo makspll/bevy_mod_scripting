makspll/bevy_mod_scripting

commit 2d9fba8216076ad285ad9f9bb922cb59d62392c9

fix!: script contexts being completely overwritten on a re-load (#345)

crates/languages/bevy_mod_scripting_rhai/src/lib.rs
@@ -179,6 +179,28 @@ impl Plugin for RhaiScriptingPlugin {
     }
 }
 
+// NEW helper function to load content into an existing context without clearing previous definitions.
+fn load_rhai_content_into_context(
+    context: &mut RhaiScriptContext,
+    script: &ScriptId,
+    content: &[u8],
+    initializers: &[ContextInitializer<RhaiScriptingPlugin>],
+    pre_handling_initializers: &[ContextPreHandlingInitializer<RhaiScriptingPlugin>],
+    runtime: &mut RhaiRuntime,
+) -> Result<(), ScriptError> {
+    let mut ast = runtime.compile(std::str::from_utf8(content)?)?;
+    ast.set_source(script.to_string());
+    initializers
+        .iter()
+        .try_for_each(|init| init(script, context))?;
+    pre_handling_initializers
+        .iter()
+        .try_for_each(|init| init(script, Entity::from_raw(0), context))?;
+    runtime.eval_ast_with_scope(&mut context.scope, &ast)?;
+    // Unlike before, do not clear statements so that definitions persist.
+    Ok(())
+}
+
 /// Load a rhai context from a script.
 pub fn rhai_context_load(
     script: &ScriptId,
@@ -187,29 +209,23 @@ pub fn rhai_context_load(
     pre_handling_initializers: &[ContextPreHandlingInitializer<RhaiScriptingPlugin>],
     runtime: &mut RhaiRuntime,
 ) -> Result<RhaiScriptContext, ScriptError> {
-    let mut ast = runtime.compile(std::str::from_utf8(content)?)?;
-    ast.set_source(script.to_string());
-
     let mut context = RhaiScriptContext {
-        ast,
+        // Using an empty AST as a placeholder.
+        ast: AST::empty(),
         scope: Scope::new(),
     };
-    initializers
-        .iter()
-        .try_for_each(|init| init(script, &mut context))?;
-
-    pre_handling_initializers
-        .iter()
-        .try_for_each(|init| init(script, Entity::from_raw(0), &mut context))?;
-
-    runtime.eval_ast_with_scope(&mut context.scope, &context.ast)?;
-    // do not invoke top level statements after the first time we run the script
-    context.ast.clear_statements();
-
+    load_rhai_content_into_context(
+        &mut context,
+        script,
+        content,
+        initializers,
+        pre_handling_initializers,
+        runtime,
+    )?;
     Ok(context)
 }
 
-/// Reload a rhai context from a script.
+/// Reload a rhai context from a script. New content is appended to the existing context.
 pub fn rhai_context_reload(
     script: &ScriptId,
     content: &[u8],
@@ -218,14 +234,14 @@ pub fn rhai_context_reload(
     pre_handling_initializers: &[ContextPreHandlingInitializer<RhaiScriptingPlugin>],
     runtime: &mut RhaiRuntime,
 ) -> Result<(), ScriptError> {
-    *context = rhai_context_load(
+    load_rhai_content_into_context(
+        context,
         script,
         content,
         initializers,
         pre_handling_initializers,
         runtime,
-    )?;
-    Ok(())
+    )
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -276,5 +292,47 @@ pub fn rhai_callback_handler(
                 Err(ScriptError::from(e))
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_reload_doesnt_overwrite_old_context() {
+        let mut runtime = RhaiRuntime::new();
+        let script_id = ScriptId::from("asd.rhai");
+        let initializers: Vec<ContextInitializer<RhaiScriptingPlugin>> = vec![];
+        let pre_handling_initializers: Vec<ContextPreHandlingInitializer<RhaiScriptingPlugin>> =
+            vec![];
+
+        // Load first content defining a function that returns 42.
+        let mut context = rhai_context_load(
+            &script_id,
+            b"let hello = 2;",
+            &initializers,
+            &pre_handling_initializers,
+            &mut runtime,
+        )
+        .unwrap();
+
+        // Reload with additional content defining a second function that returns 24.
+        rhai_context_reload(
+            &script_id,
+            b"let hello2 = 3",
+            &mut context,
+            &initializers,
+            &pre_handling_initializers,
+            &mut runtime,
+        )
+        .unwrap();
+
+        // get first var
+        let hello = context.scope.get_value::<i64>("hello").unwrap();
+        assert_eq!(hello, 2);
+        // get second var
+        let hello2 = context.scope.get_value::<i64>("hello2").unwrap();
+        assert_eq!(hello2, 3);
     }
 }

crates/languages/bevy_mod_scripting_lua/src/lib.rs
@@ -156,6 +156,30 @@ impl Plugin for LuaScriptingPlugin {
         self.scripting_plugin.finish(app);
     }
 }
+
+fn load_lua_content_into_context(
+    context: &mut Lua,
+    script_id: &ScriptId,
+    content: &[u8],
+    initializers: &[ContextInitializer<LuaScriptingPlugin>],
+    pre_handling_initializers: &[ContextPreHandlingInitializer<LuaScriptingPlugin>],
+) -> Result<(), ScriptError> {
+    initializers
+        .iter()
+        .try_for_each(|init| init(script_id, context))?;
+
+    pre_handling_initializers
+        .iter()
+        .try_for_each(|init| init(script_id, Entity::from_raw(0), context))?;
+
+    context
+        .load(content)
+        .exec()
+        .map_err(ScriptError::from_mlua_error)?;
+
+    Ok(())
+}
+
 #[profiling::function]
 /// Load a lua context from a script
 pub fn lua_context_load(
@@ -170,21 +194,16 @@ pub fn lua_context_load(
     #[cfg(not(feature = "unsafe_lua_modules"))]
     let mut context = Lua::new();
 
-    initializers
-        .iter()
-        .try_for_each(|init| init(script_id, &mut context))?;
-
-    pre_handling_initializers
-        .iter()
-        .try_for_each(|init| init(script_id, Entity::from_raw(0), &mut context))?;
-
-    context
-        .load(content)
-        .exec()
-        .map_err(ScriptError::from_mlua_error)?;
-
+    load_lua_content_into_context(
+        &mut context,
+        script_id,
+        content,
+        initializers,
+        pre_handling_initializers,
+    )?;
     Ok(context)
 }
+
 #[profiling::function]
 /// Reload a lua context from a script
 pub fn lua_context_reload(
@@ -195,12 +214,12 @@ pub fn lua_context_reload(
     pre_handling_initializers: &[ContextPreHandlingInitializer<LuaScriptingPlugin>],
     _: &mut (),
 ) -> Result<(), ScriptError> {
-    *old_ctxt = lua_context_load(
+    load_lua_content_into_context(
+        old_ctxt,
         script,
         content,
         initializers,
         pre_handling_initializers,
-        &mut (),
     )?;
     Ok(())
 }
@@ -242,4 +261,50 @@ pub fn lua_handler(
 
     let out = handler.call::<LuaScriptValue>(input)?;
     Ok(out.into())
+}
+
+#[cfg(test)]
+mod test {
+    use mlua::Value;
+
+    use super::*;
+
+    #[test]
+    fn test_reload_doesnt_overwrite_old_context() {
+        let lua = Lua::new();
+        let script_id = ScriptId::from("asd.lua");
+        let initializers = vec![];
+        let pre_handling_initializers = vec![];
+        let mut old_ctxt = lua.clone();
+
+        lua_context_load(
+            &script_id,
+            "function hello_world_from_first_load()
+            
+            end"
+            .as_bytes(),
+            &initializers,
+            &pre_handling_initializers,
+            &mut (),
+        )
+        .unwrap();
+
+        lua_context_reload(
+            &script_id,
+            "function hello_world_from_second_load()
+            
+            end"
+            .as_bytes(),
+            &mut old_ctxt,
+            &initializers,
+            &pre_handling_initializers,
+            &mut (),
+        )
+        .unwrap();
+
+        // assert both functions exist in globals
+        let globals = lua.globals();
+        assert!(globals.get::<Value>("hello_world_from_first_load").is_ok());
+        assert!(globals.get::<Value>("hello_world_from_second_load").is_ok());
+    }
 }

commit e40db41451a4c4f21698dd6dc073eb19d99e2bb1

fix: rhai reloading behavior regression from #345 [SKIP_CHANGELOG] (#351)
fix: fix issues with rhai after re-loading each context properly

crates/languages/bevy_mod_scripting_rhai/src/lib.rs
@@ -188,16 +188,18 @@ fn load_rhai_content_into_context(
     pre_handling_initializers: &[ContextPreHandlingInitializer<RhaiScriptingPlugin>],
     runtime: &mut RhaiRuntime,
 ) -> Result<(), ScriptError> {
-    let mut ast = runtime.compile(std::str::from_utf8(content)?)?;
-    ast.set_source(script.to_string());
+    context.ast = runtime.compile(std::str::from_utf8(content)?)?;
+    context.ast.set_source(script.to_string());
+
     initializers
         .iter()
         .try_for_each(|init| init(script, context))?;
     pre_handling_initializers
         .iter()
         .try_for_each(|init| init(script, Entity::from_raw(0), context))?;
-    runtime.eval_ast_with_scope(&mut context.scope, &ast)?;
-    // Unlike before, do not clear statements so that definitions persist.
+    runtime.eval_ast_with_scope(&mut context.scope, &context.ast)?;
+
+    context.ast.clear_statements();
     Ok(())
 }
 

crates/testing_crates/script_integration_test_harness/src/lib.rs
@@ -22,6 +22,7 @@ use bevy_mod_scripting_core::{
     asset::ScriptAsset,
     bindings::{pretty_print::DisplayWithWorld, script_value::ScriptValue, WorldGuard},
     callback_labels,
+    error::{InteropError, ScriptError},
     event::{IntoCallbackLabel, ScriptErrorEvent},
     extractors::{HandlerContext, WithWorldGuard},
     handler::handle_script_errors,
@@ -43,13 +44,14 @@ struct TestCallbackBuilder<P: IntoScriptPluginParams, L: IntoCallbackLabel> {
 }
 
 impl<L: IntoCallbackLabel, P: IntoScriptPluginParams> TestCallbackBuilder<P, L> {
-    fn build(script_id: impl Into<ScriptId>) -> SystemConfigs {
+    fn build(script_id: impl Into<ScriptId>, expect_response: bool) -> SystemConfigs {
         let script_id = script_id.into();
         IntoSystem::into_system(
             move |world: &mut World,
                   system_state: &mut SystemState<WithWorldGuard<HandlerContext<P>>>| {
                 let with_guard = system_state.get_mut(world);
-                run_test_callback::<P, L>(&script_id.clone(), with_guard);
+                let _ = run_test_callback::<P, L>(&script_id.clone(), with_guard, expect_response);
+
                 system_state.apply(world);
             },
         )
@@ -103,13 +105,22 @@ pub fn execute_integration_test<
         *handle = server.load(script_id.to_owned());
     };
 
+    // tests can opt in to this via "__RETURN"
+    let expect_callback_response = script_id.contains("__RETURN");
+
     app.add_systems(Startup, load_system);
-    app.add_systems(Update, TestCallbackBuilder::<P, OnTest>::build(script_id));
+    app.add_systems(
+        Update,
+        TestCallbackBuilder::<P, OnTest>::build(script_id, expect_callback_response),
+    );
     app.add_systems(
         PostUpdate,
-        TestCallbackBuilder::<P, OnTestPostUpdate>::build(script_id),
+        TestCallbackBuilder::<P, OnTestPostUpdate>::build(script_id, expect_callback_response),
     );
-    app.add_systems(Last, TestCallbackBuilder::<P, OnTestLast>::build(script_id));
+    app.add_systems(
+        Last,
+        TestCallbackBuilder::<P, OnTestLast>::build(script_id, expect_callback_response),
+    );
     app.add_systems(Update, dummy_update_system);
     app.add_systems(Startup, dummy_startup_system::<String>);
 
@@ -147,11 +158,12 @@ pub fn execute_integration_test<
 fn run_test_callback<P: IntoScriptPluginParams, C: IntoCallbackLabel>(
     script_id: &str,
     mut with_guard: WithWorldGuard<'_, '_, HandlerContext<'_, P>>,
-) {
+    expect_response: bool,
+) -> Result<ScriptValue, ScriptError> {
     let (guard, handler_ctxt) = with_guard.get_mut();
 
     if !handler_ctxt.is_script_fully_loaded(script_id.to_string().into()) {
-        return;
+        return Ok(ScriptValue::Unit);
     }
 
     let res = handler_ctxt.call::<C>(
@@ -163,14 +175,20 @@ fn run_test_callback<P: IntoScriptPluginParams, C: IntoCallbackLabel>(
     let e = match res {
         Ok(ScriptValue::Error(e)) => e.into(),
         Err(e) => e,
-        _ => {
-            match guard.with_resource_mut(|mut events: Mut<Events<TestEventFinished>>| {
-                events.send(TestEventFinished)
-            }) {
-                Ok(_) => return,
-                Err(e) => e.into(),
+        Ok(v) => {
+            if expect_response && !matches!(v, ScriptValue::Bool(true)) {
+                InteropError::external_error(format!("Response from callback {} was either not received or wasn't correct. Expected true, got: {v:?}", C::into_callback_label()).into()).into()
+            } else {
+                match guard.with_resource_mut(|mut events: Mut<Events<TestEventFinished>>| {
+                    events.send(TestEventFinished)
+                }) {
+                    Ok(_) => return Ok(v),
+                    Err(e) => e.into(),
+                }
             }
         }
     };
-    handle_script_errors(guard, vec![e].into_iter())
+    handle_script_errors(guard, vec![e.clone()].into_iter());
+
+    Err(e)
 }

assets/tests/api_availability/api_available_on_callback.lua
@@ -1,5 +1,0 @@
-function on_test()
-    assert(world ~= nil, "World was not found")
-    assert(world.get_type_by_name("TestComponent") ~= nil, "Could not find TestComponent type")
-    Entity.from_raw(1)
-end

assets/tests/api_availability/api_available_on_callback.rhai
@@ -1,5 +1,0 @@
-fn on_test() {
-    assert!(type_of(world) != "()", "World was not found");
-    assert!(type_of(world.get_type_by_name.call("TestComponent")) != "()", "Could not find TestComponent type");
-    Entity.from_raw.call(1);
-}

assets/tests/api_availability/api_available_on_callback_and_on_load__RETURN.lua
@@ -1,0 +1,22 @@
+assert(world ~= nil, "World was not found")
+assert(world.get_type_by_name("TestComponent") ~= nil, "Could not find TestComponent type")
+local global_invocation = Entity.from_raw(1)
+
+function on_test()
+    assert(world ~= nil, "World was not found")
+    assert(world.get_type_by_name("TestComponent") ~= nil, "Could not find TestComponent type")
+    Entity.from_raw(1)
+
+    -- assert global_invocation happened
+    assert(global_invocation ~= nil, "Global invocation did not happen")
+
+    return true
+end
+
+function on_test_post_update()
+    return true
+end
+
+function on_test_last()
+    return true
+end

assets/tests/api_availability/api_available_on_callback_and_on_load__RETURN.rhai
@@ -1,0 +1,22 @@
+assert(type_of(world) != "()", "World was not found");
+assert(type_of(world.get_type_by_name.call("TestComponent")) != "()", "Could not find TestComponent type");
+let global_invocation = Entity.from_raw.call(1);
+
+fn on_test(){
+    assert(type_of(world) != "()", "World was not found");
+    assert(type_of(world.get_type_by_name.call("TestComponent")) != "()", "Could not find TestComponent type");
+    Entity.from_raw.call(1);
+
+    // assert global_invocation happened
+    assert(type_of(global_invocation) != "()", "Global invocation did not happen");
+
+    return true;
+}
+
+fn on_test_post_update(){
+    return true;
+}
+
+fn on_test_last(){
+    return true;
+}

assets/tests/api_availability/api_available_on_script_load.lua
@@ -1,3 +1,0 @@
-assert(world ~= nil, "World was not found")
-assert(world.get_type_by_name("TestComponent") ~= nil, "Could not find TestComponent type")
-Entity.from_raw(1)

assets/tests/api_availability/api_available_on_script_load.rhai
@@ -1,3 +1,0 @@
-assert!(type_of(world) != "()", "World was not found");
-assert!(type_of(world.get_type_by_name.call("TestComponent")) != "()", "Could not find TestComponent type");
-let out = Entity.from_raw.call(1);

assets/tests/pop/vec.rhai
@@ -3,4 +3,4 @@ let res = world.get_resource.call(res_type);
 
 let popped = res.vec_usize.pop.call();
 
-assert(popped == 5, "Pop did not work");
+assert(popped == 5, "Pop did not work, got " + popped);