0431c82ff0a936def69c509d2a3410fe5b438cdd
makspll <makspl17@gmail.com>
2025-03-23 19:49:16 +0000

    chore: add dynamic component query tests

---
+++ assets/tests/query/dynamic_component_query.lua
@@ -0,0 +1,20 @@
+local entity_a = world.spawn()
+local NewComponent = world.register_new_component("NewComponent")
+
+world.add_default_component(entity_a, NewComponent)
+
+local found_entities = {}
+for i,result in pairs(world.query():component(NewComponent):build()) do
+    table.insert(found_entities, result:entity())
+end
+
+assert(#found_entities == 1, "Expected 1 entities, got " .. #found_entities)
+
+expected_entities = {
+    entity_a
+}
+
+for i, entity in ipairs(found_entities) do
+    assert(entity:index() == expected_entities[i]:index(), "Expected entity " .. expected_entities[i]:index() .. " but got " .. entity:index())
+end
+
