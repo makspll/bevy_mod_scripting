eca2445a35fcef68669700ed2f3414b760501955
makspll <makspl17@gmail.com>
2025-07-03 13:01:21 +0100

    trigger ci

--- crates/bevy_mod_scripting_functions/src/lib.rs
+++ crates/bevy_mod_scripting_functions/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(missing_docs)]
 
-use ::bevy::prelude::*;
+use bevy::prelude::*;
 pub mod bevy_bindings;
 pub mod core;
 pub use core::*;
@@ -14,7 +14,7 @@ impl Plugin for ScriptFunctionsPlugin {
         register_bevy_bindings(app);
         register_core_functions(app);
 
-        // TODO: if bevy ever does this itself we should remove this
+        // TODO: if bevy ever does this itself we should remove this.
         app.world_mut().register_component::<Parent>();
         app.world_mut().register_component::<Children>();
     }
