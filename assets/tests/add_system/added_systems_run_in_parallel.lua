e8a78ead5c402c19504f6bc982fcac98fe199c89
makspll <makspl17@gmail.com>
2025-08-12 20:14:44 +0100

    fix test

--- assets/tests/add_system/added_systems_run_in_parallel.lua
+++ assets/tests/add_system/added_systems_run_in_parallel.lua
@@ -22,21 +22,30 @@ function on_test()
 
   local expected_dot_graph = [[
 digraph {
-  node_0 [label="bevy_mod_scripting_core::bindings::allocator::garbage_collector"];
-  node_1 [label="script_integration_test_harness::dummy_before_post_update_system"];
-  node_2 [label="script_integration_test_harness::dummy_post_update_system"];
-  node_3 [label="on_test_post_update"];
-  node_4 [label="custom_system_a"];
-  node_5 [label="custom_system_b"];
-  node_6 [label="SystemSet GarbageCollection"];
-  node_7 [label="SystemSet ScriptSystem(custom_system_a)"];
-  node_8 [label="SystemSet ScriptSystem(custom_system_b)"];
-  node_0 -> node_6 [color=red, label="child of", arrowhead=diamond];
-  node_4 -> node_7 [color=red, label="child of", arrowhead=diamond];
-  node_5 -> node_8 [color=red, label="child of", arrowhead=diamond];
-  node_1 -> node_2 [color=blue, label="runs before", arrowhead=normal];
-  node_3 -> node_4 [color=blue, label="runs before", arrowhead=normal];
-  node_3 -> node_5 [color=blue, label="runs before", arrowhead=normal];
+  node_0 [label="bevy_asset::assets::Assets<bevy_asset::folder::LoadedFolder>::asset_events"];
+  node_1 [label="bevy_asset::assets::Assets<bevy_asset::assets::LoadedUntypedAsset>::asset_events"];
+  node_2 [label="bevy_asset::assets::Assets<()>::asset_events"];
+  node_3 [label="bevy_asset::assets::Assets<bevy_mod_scripting_core::asset::ScriptAsset>::asset_events"];
+  node_4 [label="bevy_mod_scripting_core::bindings::allocator::garbage_collector"];
+  node_5 [label="script_integration_test_harness::dummy_before_post_update_system"];
+  node_6 [label="script_integration_test_harness::dummy_post_update_system"];
+  node_7 [label="on_test_post_update"];
+  node_8 [label="custom_system_a"];
+  node_9 [label="custom_system_b"];
+  node_10 [label="SystemSet AssetEvents"];
+  node_11 [label="SystemSet GarbageCollection"];
+  node_12 [label="SystemSet ScriptSystem(custom_system_a)"];
+  node_13 [label="SystemSet ScriptSystem(custom_system_b)"];
+  node_0 -> node_10 [color=red, label="child of", arrowhead=diamond];
+  node_1 -> node_10 [color=red, label="child of", arrowhead=diamond];
+  node_2 -> node_10 [color=red, label="child of", arrowhead=diamond];
+  node_3 -> node_10 [color=red, label="child of", arrowhead=diamond];
+  node_4 -> node_11 [color=red, label="child of", arrowhead=diamond];
+  node_8 -> node_12 [color=red, label="child of", arrowhead=diamond];
+  node_9 -> node_13 [color=red, label="child of", arrowhead=diamond];
+  node_5 -> node_6 [color=blue, label="runs before", arrowhead=normal];
+  node_7 -> node_8 [color=blue, label="runs before", arrowhead=normal];
+  node_7 -> node_9 [color=blue, label="runs before", arrowhead=normal];
 }
     ]]
   assert_str_eq(dot_graph, expected_dot_graph, "Expected the schedule graph to match the expected graph")
