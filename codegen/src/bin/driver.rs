0a266d85a41dfb4c9c71fb9c1612b3f715c1db3a
makspll <makspl17@gmail.com>
2025-08-23 11:42:04 +0100

    more imports

--- codegen/src/bin/driver.rs
+++ codegen/src/bin/driver.rs
@@ -1,5 +1,5 @@
 #![feature(rustc_private)]
-use bevy_api_gen::{driver::driver_main, *};
+use bevy_mod_scripting_codegen::{driver::driver_main, *};
 
 fn main() {
     // initially set it to high so no logs are missed, but later when we parse the args we will set it to the correct level
