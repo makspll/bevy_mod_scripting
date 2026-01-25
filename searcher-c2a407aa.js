837bb72522f856ab68639eba63a9bfeea006b6ac
makspll <makspll@users.noreply.github.com>
2026-01-25 12:21:41 +0000

    deploy: 03a97af809fcf232cc9daace35c14255204ec83a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2c21a252.js',
+                path_to_root + 'searchindex-857fc6eb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
