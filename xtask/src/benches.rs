30aac379ad4267fabe66fc8e90af65f4978bc6b9
makspll <makspl17@gmail.com>
2026-07-29 20:31:53 +0100

    fix bench compilation time

--- xtask/src/benches.rs
+++ xtask/src/benches.rs
@@ -5,11 +5,18 @@ use regex::Regex;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+struct Phase {
+    start: f64,
+    end: f64,
+}
+
+#[derive(Debug, Deserialize)]
 struct Unit {
     name: String,
     target: String,
     duration: f64,
-    rmeta_time: Option<f64>,
+    #[serde(default)]
+    sections: Option<Vec<(String, Phase)>>, // new format
 }
 
 /// Extract `const UNIT_DATA = [...]`
@@ -40,25 +47,32 @@ fn convert(units: Vec<Unit>, include_crates: Vec<String>) -> String {
             continue;
         }
 
-        let rmeta_time = if let Some(rmeta_time) = u.rmeta_time {
-            rmeta_time
-        } else {
-            continue;
-        };
+        let frontend = u.sections
+            .iter()
+            .flat_map(|i| i.iter())
+            .find(|(n, _)| n == "frontend")
+            .map(|(_, p)| p.end - p.start);
 
-        // Phase split (best-effort)
-        let total_compile_time_s = u.duration;
-        let codegen_time = u.duration - rmeta_time;
+        let codegen = u.sections
+            .iter()
+            .flat_map(|i| i.iter())
+            .find(|(n, _)| n == "codegen")
+            .map(|(_, p)| p.end - p.start);
+
 
         let meta_name = format!("{}::blocking_comp", crate_name);
         let codegen_name = format!("{}::codegen", crate_name);
         let total_time_name = format!("{}::total", crate_name);
-        total_rmeta += rmeta_time;
-        total_codegen += codegen_time;
 
-        write_metric(&mut out, &meta_name, rmeta_time);
-        write_metric(&mut out, &codegen_name, codegen_time);
-        write_metric(&mut out, &total_time_name, total_compile_time_s);
+        if let Some(frontend) = frontend {
+            write_metric(&mut out, &meta_name, frontend);
+            total_rmeta += frontend;
+        }
+        if let Some(codegen) = codegen {
+            write_metric(&mut out, &codegen_name, codegen);
+            total_codegen += codegen;
+        }
+        write_metric(&mut out, &total_time_name, u.duration);
     }
     write_metric(&mut out, &format!("total::blocking_comp"), total_rmeta);
     write_metric(&mut out, &format!("total::codegen"), total_codegen);
@@ -150,78 +164,78 @@ mod test {
         );
 
         let metrics = res.unwrap();
-        let expected = r#"build_time/bevy_mod_scripting_world::blocking_comp   time:   [1.150 s 1.150 s 1.150 s]
-build_time/bevy_mod_scripting_world::codegen   time:   [0.340 s 0.340 s 0.340 s]
-build_time/bevy_mod_scripting_world::total   time:   [1.490 s 1.490 s 1.490 s]
-build_time/bevy_mod_scripting_display::blocking_comp   time:   [1.390 s 1.390 s 1.390 s]
-build_time/bevy_mod_scripting_display::codegen   time:   [0.540 s 0.540 s 0.540 s]
-build_time/bevy_mod_scripting_display::total   time:   [1.930 s 1.930 s 1.930 s]
-build_time/bevy_mod_scripting_bindings_domain::blocking_comp   time:   [0.720 s 0.720 s 0.720 s]
-build_time/bevy_mod_scripting_bindings_domain::codegen   time:   [0.220 s 0.220 s 0.220 s]
-build_time/bevy_mod_scripting_bindings_domain::total   time:   [0.940 s 0.940 s 0.940 s]
-build_time/bevy_mod_scripting_asset::blocking_comp   time:   [1.870 s 1.870 s 1.870 s]
-build_time/bevy_mod_scripting_asset::codegen   time:   [0.850 s 0.850 s 0.850 s]
-build_time/bevy_mod_scripting_asset::total   time:   [2.720 s 2.720 s 2.720 s]
-build_time/ladfile::blocking_comp   time:   [3.570 s 3.570 s 3.570 s]
-build_time/ladfile::codegen   time:   [6.840 s 6.840 s 6.840 s]
-build_time/ladfile::total   time:   [10.410 s 10.410 s 10.410 s]
-build_time/bevy_mod_scripting_script::blocking_comp   time:   [1.410 s 1.410 s 1.410 s]
-build_time/bevy_mod_scripting_script::codegen   time:   [2.830 s 2.830 s 2.830 s]
-build_time/bevy_mod_scripting_script::total   time:   [4.240 s 4.240 s 4.240 s]
-build_time/bevy_mod_scripting_bindings::blocking_comp   time:   [24.070 s 24.070 s 24.070 s]
-build_time/bevy_mod_scripting_bindings::codegen   time:   [8.880 s 8.880 s 8.880 s]
-build_time/bevy_mod_scripting_bindings::total   time:   [32.950 s 32.950 s 32.950 s]
-build_time/lua_language_server_lad_backend::blocking_comp   time:   [3.500 s 3.500 s 3.500 s]
-build_time/lua_language_server_lad_backend::codegen   time:   [4.230 s 4.230 s 4.230 s]
-build_time/lua_language_server_lad_backend::total   time:   [7.730 s 7.730 s 7.730 s]
-build_time/mdbook_lad_preprocessor::blocking_comp   time:   [3.880 s 3.880 s 3.880 s]
-build_time/mdbook_lad_preprocessor::codegen   time:   [4.890 s 4.890 s 4.890 s]
-build_time/mdbook_lad_preprocessor::total   time:   [8.770 s 8.770 s 8.770 s]
-build_time/bevy_mod_scripting_core::blocking_comp   time:   [8.140 s 8.140 s 8.140 s]
-build_time/bevy_mod_scripting_core::codegen   time:   [32.510 s 32.510 s 32.510 s]
-build_time/bevy_mod_scripting_core::total   time:   [40.650 s 40.650 s 40.650 s]
-build_time/bevy_math_bms_bindings::blocking_comp   time:   [15.110 s 15.110 s 15.110 s]
-build_time/bevy_math_bms_bindings::codegen   time:   [111.500 s 111.500 s 111.500 s]
-build_time/bevy_math_bms_bindings::total   time:   [126.610 s 126.610 s 126.610 s]
-build_time/bevy_mod_scripting_rhai::blocking_comp   time:   [9.320 s 9.320 s 9.320 s]
-build_time/bevy_mod_scripting_rhai::codegen   time:   [65.510 s 65.510 s 65.510 s]
-build_time/bevy_mod_scripting_rhai::total   time:   [74.830 s 74.830 s 74.830 s]
-build_time/bevy_reflect_bms_bindings::blocking_comp   time:   [81.840 s 81.840 s 81.840 s]
-build_time/bevy_reflect_bms_bindings::codegen   time:   [442.670 s 442.670 s 442.670 s]
-build_time/bevy_reflect_bms_bindings::total   time:   [524.510 s 524.510 s 524.510 s]
-build_time/bevy_input_bms_bindings::blocking_comp   time:   [5.830 s 5.830 s 5.830 s]
-build_time/bevy_input_bms_bindings::codegen   time:   [70.850 s 70.850 s 70.850 s]
-build_time/bevy_input_bms_bindings::total   time:   [76.680 s 76.680 s 76.680 s]
-build_time/bevy_color_bms_bindings::blocking_comp   time:   [4.420 s 4.420 s 4.420 s]
-build_time/bevy_color_bms_bindings::codegen   time:   [43.690 s 43.690 s 43.690 s]
-build_time/bevy_color_bms_bindings::total   time:   [48.110 s 48.110 s 48.110 s]
-build_time/bevy_time_bms_bindings::blocking_comp   time:   [2.290 s 2.290 s 2.290 s]
-build_time/bevy_time_bms_bindings::codegen   time:   [27.680 s 27.680 s 27.680 s]
-build_time/bevy_time_bms_bindings::total   time:   [29.970 s 29.970 s 29.970 s]
-build_time/bevy_transform_bms_bindings::blocking_comp   time:   [3.430 s 3.430 s 3.430 s]
-build_time/bevy_transform_bms_bindings::codegen   time:   [33.310 s 33.310 s 33.310 s]
-build_time/bevy_transform_bms_bindings::total   time:   [36.740 s 36.740 s 36.740 s]
-build_time/bevy_ecs_bms_bindings::blocking_comp   time:   [3.480 s 3.480 s 3.480 s]
-build_time/bevy_ecs_bms_bindings::codegen   time:   [35.010 s 35.010 s 35.010 s]
-build_time/bevy_ecs_bms_bindings::total   time:   [38.490 s 38.490 s 38.490 s]
-build_time/bevy_core_pipeline_bms_bindings::blocking_comp   time:   [4.270 s 4.270 s 4.270 s]
-build_time/bevy_core_pipeline_bms_bindings::codegen   time:   [19.850 s 19.850 s 19.850 s]
-build_time/bevy_core_pipeline_bms_bindings::total   time:   [24.120 s 24.120 s 24.120 s]
-build_time/ladfile_builder::blocking_comp   time:   [2.940 s 2.940 s 2.940 s]
-build_time/ladfile_builder::codegen   time:   [13.500 s 13.500 s 13.500 s]
-build_time/ladfile_builder::total   time:   [16.440 s 16.440 s 16.440 s]
-build_time/bevy_mod_scripting_functions::blocking_comp   time:   [12.570 s 12.570 s 12.570 s]
-build_time/bevy_mod_scripting_functions::codegen   time:   [55.070 s 55.070 s 55.070 s]
-build_time/bevy_mod_scripting_functions::total   time:   [67.640 s 67.640 s 67.640 s]
-build_time/script_integration_test_harness::blocking_comp   time:   [29.130 s 29.130 s 29.130 s]
-build_time/script_integration_test_harness::codegen   time:   [21.660 s 21.660 s 21.660 s]
-build_time/script_integration_test_harness::total   time:   [50.790 s 50.790 s 50.790 s]
-build_time/bevy_mod_scripting::blocking_comp   time:   [5.700 s 5.700 s 5.700 s]
-build_time/bevy_mod_scripting::codegen   time:   [0.650 s 0.650 s 0.650 s]
-build_time/bevy_mod_scripting::total   time:   [6.350 s 6.350 s 6.350 s]
-build_time/total::blocking_comp   time:   [230.030 s 230.030 s 230.030 s]
-build_time/total::codegen   time:   [1003.080 s 1003.080 s 1003.080 s]
-build_time/total::total   time:   [1233.110 s 1233.110 s 1233.110 s]
+        let expected = r#"build_time/bevy_mod_scripting_world::blocking_comp   time:   [0.680 s 0.680 s 0.680 s]
+build_time/bevy_mod_scripting_world::codegen   time:   [3.120 s 3.120 s 3.120 s]
+build_time/bevy_mod_scripting_world::total   time:   [3.800 s 3.800 s 3.800 s]
+build_time/bevy_mod_scripting_display::blocking_comp   time:   [0.700 s 0.700 s 0.700 s]
+build_time/bevy_mod_scripting_display::codegen   time:   [1.740 s 1.740 s 1.740 s]
+build_time/bevy_mod_scripting_display::total   time:   [2.440 s 2.440 s 2.440 s]
+build_time/bevy_mod_scripting_bindings_domain::blocking_comp   time:   [0.740 s 0.740 s 0.740 s]
+build_time/bevy_mod_scripting_bindings_domain::codegen   time:   [0.390 s 0.390 s 0.390 s]
+build_time/bevy_mod_scripting_bindings_domain::total   time:   [1.130 s 1.130 s 1.130 s]
+build_time/bevy_mod_scripting_asset::blocking_comp   time:   [0.990 s 0.990 s 0.990 s]
+build_time/bevy_mod_scripting_asset::codegen   time:   [1.900 s 1.900 s 1.900 s]
+build_time/bevy_mod_scripting_asset::total   time:   [2.890 s 2.890 s 2.890 s]
+build_time/bevy_mod_scripting_script::blocking_comp   time:   [0.610 s 0.610 s 0.610 s]
+build_time/bevy_mod_scripting_script::codegen   time:   [9.880 s 9.880 s 9.880 s]
+build_time/bevy_mod_scripting_script::total   time:   [10.490 s 10.490 s 10.490 s]
+build_time/bevy_mod_scripting_bindings::blocking_comp   time:   [17.610 s 17.610 s 17.610 s]
+build_time/bevy_mod_scripting_bindings::codegen   time:   [61.200 s 61.200 s 61.200 s]
+build_time/bevy_mod_scripting_bindings::total   time:   [78.810 s 78.810 s 78.810 s]
+build_time/bevy_mod_scripting_core::blocking_comp   time:   [5.310 s 5.310 s 5.310 s]
+build_time/bevy_mod_scripting_core::codegen   time:   [43.800 s 43.800 s 43.800 s]
+build_time/bevy_mod_scripting_core::total   time:   [49.110 s 49.110 s 49.110 s]
+build_time/ladfile::blocking_comp   time:   [1.620 s 1.620 s 1.620 s]
+build_time/ladfile::codegen   time:   [16.620 s 16.620 s 16.620 s]
+build_time/ladfile::total   time:   [18.240 s 18.240 s 18.240 s]
+build_time/bevy_mod_scripting_rhai::blocking_comp   time:   [4.380 s 4.380 s 4.380 s]
+build_time/bevy_mod_scripting_rhai::codegen   time:   [78.440 s 78.440 s 78.440 s]
+build_time/bevy_mod_scripting_rhai::total   time:   [82.820 s 82.820 s 82.820 s]
+build_time/bevy_core_pipeline_bms_bindings::blocking_comp   time:   [1.060 s 1.060 s 1.060 s]
+build_time/bevy_core_pipeline_bms_bindings::codegen   time:   [18.890 s 18.890 s 18.890 s]
+build_time/bevy_core_pipeline_bms_bindings::total   time:   [19.950 s 19.950 s 19.950 s]
+build_time/bevy_time_bms_bindings::blocking_comp   time:   [0.940 s 0.940 s 0.940 s]
+build_time/bevy_time_bms_bindings::codegen   time:   [27.640 s 27.640 s 27.640 s]
+build_time/bevy_time_bms_bindings::total   time:   [28.580 s 28.580 s 28.580 s]
+build_time/bevy_color_bms_bindings::blocking_comp   time:   [1.770 s 1.770 s 1.770 s]
+build_time/bevy_color_bms_bindings::codegen   time:   [46.810 s 46.810 s 46.810 s]
+build_time/bevy_color_bms_bindings::total   time:   [48.580 s 48.580 s 48.580 s]
+build_time/bevy_ecs_bms_bindings::blocking_comp   time:   [1.320 s 1.320 s 1.320 s]
+build_time/bevy_ecs_bms_bindings::codegen   time:   [40.040 s 40.040 s 40.040 s]
+build_time/bevy_ecs_bms_bindings::total   time:   [41.360 s 41.360 s 41.360 s]
+build_time/bevy_transform_bms_bindings::blocking_comp   time:   [1.460 s 1.460 s 1.460 s]
+build_time/bevy_transform_bms_bindings::codegen   time:   [33.890 s 33.890 s 33.890 s]
+build_time/bevy_transform_bms_bindings::total   time:   [35.350 s 35.350 s 35.350 s]
+build_time/bevy_math_bms_bindings::blocking_comp   time:   [6.310 s 6.310 s 6.310 s]
+build_time/bevy_math_bms_bindings::codegen   time:   [179.600 s 179.600 s 179.600 s]
+build_time/bevy_math_bms_bindings::total   time:   [185.910 s 185.910 s 185.910 s]
+build_time/bevy_input_bms_bindings::blocking_comp   time:   [2.340 s 2.340 s 2.340 s]
+build_time/bevy_input_bms_bindings::codegen   time:   [89.130 s 89.130 s 89.130 s]
+build_time/bevy_input_bms_bindings::total   time:   [91.470 s 91.470 s 91.470 s]
+build_time/bevy_reflect_bms_bindings::blocking_comp   time:   [35.240 s 35.240 s 35.240 s]
+build_time/bevy_reflect_bms_bindings::codegen   time:   [397.330 s 397.330 s 397.330 s]
+build_time/bevy_reflect_bms_bindings::total   time:   [432.570 s 432.570 s 432.570 s]
+build_time/lua_language_server_lad_backend::blocking_comp   time:   [1.130 s 1.130 s 1.130 s]
+build_time/lua_language_server_lad_backend::codegen   time:   [9.310 s 9.310 s 9.310 s]
+build_time/lua_language_server_lad_backend::total   time:   [10.440 s 10.440 s 10.440 s]
+build_time/bevy_mod_scripting_functions::blocking_comp   time:   [4.310 s 4.310 s 4.310 s]
+build_time/bevy_mod_scripting_functions::codegen   time:   [120.900 s 120.900 s 120.900 s]
+build_time/bevy_mod_scripting_functions::total   time:   [125.210 s 125.210 s 125.210 s]
+build_time/ladfile_builder::blocking_comp   time:   [1.440 s 1.440 s 1.440 s]
+build_time/ladfile_builder::codegen   time:   [13.780 s 13.780 s 13.780 s]
+build_time/ladfile_builder::total   time:   [15.220 s 15.220 s 15.220 s]
+build_time/bevy_mod_scripting::blocking_comp   time:   [0.400 s 0.400 s 0.400 s]
+build_time/bevy_mod_scripting::codegen   time:   [0.560 s 0.560 s 0.560 s]
+build_time/bevy_mod_scripting::total   time:   [0.960 s 0.960 s 0.960 s]
+build_time/script_integration_test_harness::blocking_comp   time:   [5.370 s 5.370 s 5.370 s]
+build_time/script_integration_test_harness::codegen   time:   [80.190 s 80.190 s 80.190 s]
+build_time/script_integration_test_harness::total   time:   [85.560 s 85.560 s 85.560 s]
+build_time/mdbook_lad_preprocessor::blocking_comp   time:   [1.020 s 1.020 s 1.020 s]
+build_time/mdbook_lad_preprocessor::codegen   time:   [11.680 s 11.680 s 11.680 s]
+build_time/mdbook_lad_preprocessor::total   time:   [12.700 s 12.700 s 12.700 s]
+build_time/total::blocking_comp   time:   [96.750 s 96.750 s 96.750 s]
+build_time/total::codegen   time:   [1286.840 s 1286.840 s 1286.840 s]
+build_time/total::total   time:   [1383.590 s 1383.590 s 1383.590 s]
 "#;
         pretty_assertions::assert_str_eq!(metrics, expected);
     }
